37a7c675d33f9813b702d500e5108c29b29aa837
Uladzimir1367 <130497737+Uladzimir1367@users.noreply.github.com>
2024-08-10 08:50:42 +0300

    HW - 3

---
+++ seminar_6/code/src/main.function.php
@@ -0,0 +1,29 @@
+<?php
+function main(string $configFileAddress): string
+{
+    if (!($config = readConfig($configFileAddress))) {
+        return handleError("Невозможно подключить файл настроек");
+    }
+    if (!($functionName = parseCommand()) || !function_exists($functionName)) {
+        return handleError("Вызываемая функция не существует");
+    }
+    return $functionName($config);
+}
+
+function parseCommand(): ?string
+{
+    if (!isset($_SERVER['argv'][1])) {
+        return 'helpFunction';
+    }
+    $arg = $_SERVER['argv'][1];
+    return match ($arg) {
+        'read-all'      => 'readAllFunction',
+        'add'           => 'addFunction',
+        'clear'         => 'clearFunction',
+        'search'        => 'searchBD',
+        'delete'        => 'deleteUser',
+        'read-profiles' => 'readProfilesDirectory',
+        'read-profile'  => 'readProfile',
+        default         => null,
+    };
+}
